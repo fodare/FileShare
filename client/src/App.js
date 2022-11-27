ea6303fd6f94b8fdfb8d32da6e783c9e47920411
fodare <folorunsodamilare007@hotmail.com>
2022-11-27 14:55:00 +0100

    Create template view to collect and post file(s) to server for storage

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,10 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import UploadCard from "./components/UploadCard";
 
 function App() {
-   const [postdata, setPostData] = useState({});
-   const [id, setId] = useState(1);
-   const [idFromButtonClick, setIdFromButtonClick] = useState(1);
-
-   const handleClick = () => {
-      setIdFromButtonClick(id);
-   };
-
-   useEffect(() => {
-      axios
-         .get(`/api/posts/${id}`)
-         .then((res) => {
-            console.log(res.data);
-            setPostData(res.data);
-         })
-         .catch((err) => {
-            console.log(err);
-         });
-   }, [idFromButtonClick]);
-
    return (
-      <div className="App">
-         <input
-            type="text"
-            value={id}
-            onChange={(e) => {
-               setId(e.target.value);
-            }}
-         />
-         <button type="button" onClick={handleClick}>
-            Get Post
-         </button>
-         <div>
-            <p>Post id: {postdata.id}</p>
-            <p>Post userId: {postdata.userId}</p>
-            <p>Post title: {postdata.title}</p>
-            <p>Post body: {postdata.body}</p>
-         </div>
+      <div className="app-div">
+         <UploadCard />
       </div>
    );
 }
