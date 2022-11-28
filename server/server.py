3d127fb8167dc0d8e93b60164fe62a932a3419e8
fodare <folorunsodamilare007@hotmail.com>
2022-11-28 19:31:45 +0100

    Remove typicode test routes

--- server/server.py
+++ server/server.py
@@ -12,22 +12,6 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/api/posts", methods=['GET'])
-def get_posts():
-    url = 'https://jsonplaceholder.typicode.com/posts'
-    response = requests.get(url)
-    response_data = response.json()
-    return make_response(response_data, 200)
-
-
-@app.route("/api/posts/<string:id>", methods=['GET'])
-def get_post(id):
-    url = f'https://jsonplaceholder.typicode.com/posts/{id}'
-    response = requests.get(url)
-    response_data = response.json()
-    return make_response(response_data, 200)
-
-
 @app.route("/api/file/upload", methods=['POST'])
 def upload_file():
     if request.method == 'POST':
