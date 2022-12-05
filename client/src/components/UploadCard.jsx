4d1168153f73540a2e973c4e85e87258c6b93e3f
fodare <folorunsodamilare007@hotmail.com>
2022-12-05 20:02:29 +0100

    Add simple frontend file size validation before sending file backend server

--- client/src/components/UploadCard.jsx
+++ client/src/components/UploadCard.jsx
@@ -5,10 +5,9 @@ import "react-toastify/dist/ReactToastify.css";
 
 function UploadCard() {
    const [files, setFiles] = useState([]);
-   const [message, setMessage] = useState([]);
 
-   const notify = () => {
-      toast(message);
+   const notify = (toastMessage) => {
+      toast(toastMessage);
    };
 
    const onInputChange = (e) => {
@@ -16,16 +15,12 @@ function UploadCard() {
    };
 
    const onSubmit = (e) => {
-      e.preventDefault();
       const data = new FormData();
 
       for (let i = 0; i < files.length; i++) {
          if (files[i]["size"] > 70000) {
             // Tweak number here to set max file size
-            // alert(
-            //    `${files[i]["name"]} is too large! File size is ${files[i]["size"]}. Maximum files size should be 70000 bytes`
-            // );
-            setMessage(
+            notify(
                `${files[i]["name"]} is too large! File size is ${files[i]["size"]}. Maximum files size should be 70000 bytes`
             );
          } else {
@@ -34,19 +29,20 @@ function UploadCard() {
                .post(`/api/file/upload`, data)
                .then((e) => {
                   console.log(e.data, e.status);
-                  setMessage(`File uploaded successfully!`);
+                  notify(`${files[i]["name"]} uploaded successfully!`);
                })
                .catch((err) => {
-                  console.log(err);
+                  notify(`Error uploading file. ${err}`);
                });
          }
       }
+      e.preventDefault();
    };
 
    return (
       <div className="upload-card">
          <span>
-            <ToastContainer />
+            <ToastContainer theme="dark" autoClose={8000} />
          </span>
          <form
             className=""
